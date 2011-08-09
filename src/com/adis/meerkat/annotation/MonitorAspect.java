c6c10a41d348431550c1eebaf2d8ab33956184a8
Shawn Vader <shawn.vader@adis-ltd.co.uk>
2011-08-09 13:54:24 +0100

    Adding more files

---
+++ src/com/adis/meerkat/annotation/MonitorAspect.java
@@ -0,0 +1,8 @@
+package com.adis.meerkat.annotation;
+
+/**
+ * User: vader
+ * Date: 07/08/2011
+ */
+public class MonitorAspect {
+}
---
+++ src/com/adis/meerkat/aspect/MonitorAspect.java
@@ -0,0 +1,8 @@
+package com.adis.meerkat.aspect;
+
+/**
+ * User: vader
+ * Date: 07/08/2011
+ */
+public class MonitorAspect {
+}
